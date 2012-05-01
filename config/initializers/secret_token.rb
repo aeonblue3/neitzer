4025ff2be37073ed331b0ede62d51eca6e28b672
Chris Neitzer <chris@neitzer.com>
2012-04-30 20:12:42 -0400

    This is going to be my new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neitzer::Application.config.secret_token = '80796e9445968bb6a5bd19318e6548105e885b555533e4d934fd52ccdfd20ba77f38b021942f055260ad5ebc0d3f582c880a3b2940458a0daea3ed186ddb8895'
---
+++ test/unit/post_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "article is published" do
+    assert true
+  end
+end
